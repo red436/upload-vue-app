1d12e036284e65ed745cacaa2e22ae9536521262
red436 <redbeard.436@gmail.com>
2022-07-04 11:26:50 -0700

    Update upload.js

--- routes/upload.js
+++ routes/upload.js
@@ -7,7 +7,7 @@ var axios = require('axios');
 router.post('/', upload.single('video'), (req, res, next) => {
    var IP = req.get('X-Real-IP');
    res.send({"message": "Your upload was a success"});
-   axios.post('https://api.pushbullet.com/v2/pushes', { active: true, type: 'note', title: 'New video upload', body: 'New video notification, IP: ' + IP, email: 'redbeard.436@gmail.com'}, { headers: {'Access-Token' : 'o.vLD8TMCvkLFg7M4qxWRhqzSZA8XpNssg' }})
+   axios.post('https://api.pushbullet.com/v2/pushes', { active: true, type: 'note', title: 'New video upload', body: 'New video notification, IP: ' + IP, email: 'redbeard.436@gmail.com'}, { headers: {'Access-Token' : '' }})
       .then((res) => { console.log(res.data); })
       .catch((err) => { console.log(err); });
 });
